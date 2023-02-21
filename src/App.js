a5722b50e0bf8ccf017f2b43b7982c41aad359d3
SaraMoreiraG <sara_moreira@hotmail.es>
2023-02-21 21:14:48 +0100

    Simple counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Timer from './components/Timer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='row text-white justify-content-center m-1' style={{ background: '#070707' }}>
+        <div className='col-sm-1 d-flex justify-content-center align-content-center border border-secondary rounded py-5 m-2' style={{ background: '#0d0d0d' }}>
+          <div>
+            <i class="fa-regular fa-clock"></i>
+          </div>
+        </div>
+        <div className='col-sm-1 d-flex justify-content-center align-content-center border border-secondary rounded py-5 m-2' style={{ background: '#0d0d0d' }}>
+
+        </div>
+        <div className='col-sm-1 d-flex justify-content-center align-content-center border border-secondary rounded py-5 m-2' style={{ background: '#0d0d0d' }}>
+
+        </div>
+        <div className='col-sm-1 d-flex justify-content-center align-content-center border border-secondary rounded py-5 m-2' style={{ background: '#0d0d0d' }}>
+
+        </div>
+        <div className='col-sm-1 d-flex justify-content-center align-content-center border border-secondary rounded py-5 m-2' style={{ background: '#0d0d0d' }}>
+
+        </div>
+        <div className='col-sm-1 d-flex justify-content-center align-content-center border border-secondary rounded py-5 m-2' style={{ background: '#0d0d0d' }}>
+
+        </div>
+        <div className='col-sm-1 d-flex justify-content-center align-content-center border border-secondary rounded py-5 m-2' style={{ background: '#0d0d0d' }}>
+          <Timer />
+        </div>
+      </div>
     </div>
   );
 }
